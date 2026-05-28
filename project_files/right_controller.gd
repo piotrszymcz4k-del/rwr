88bee6f8676adb192c4dbbe46d7420310e649667
Piotr Szymczak <piotr.szymczak@student.poznan.put.pl>
2026-05-28 17:31:28 +0200

    Ukończony projekt: Teleportacja i lokomocja

--- project_files/right_controller.gd
+++ project_files/right_controller.gd
@@ -5,35 +5,59 @@ extends XRController3D
 var xr_origin: XROrigin3D
 var xr_camera: XRCamera3D
 
+# Zmienne do obrotu skokowego (Snap Turn)
+var turn_step: float = deg_to_rad(45.0) # Obrót o 45 stopni
+var can_snap_turn: bool = true # Blokada ciągłego obrotu
+
 func _ready() -> void:
 	xr_origin = get_parent() as XROrigin3D
 	xr_camera = xr_origin.get_node("XRCamera3D") as XRCamera3D
 	marker.visible = false
 	
-	# TA LINIJKA NASŁUCHUJE KLIKNIĘCIA PRZYCISKU:
-	self.button_pressed.connect(self._on_button_pressed)
+	# Podłączenie przycisku - teleport nastąpi przy PUSZCZENIU triggera
+	self.button_released.connect(self._on_button_released)
 
 func _process(_delta: float) -> void:
+	# 1. OBSŁUGA ZNACZNIKA TELEPORTU
 	if ray.is_colliding():
-		marker.global_transform.origin = ray.get_collision_point()
+		marker.global_position = ray.get_collision_point()
 		marker.visible = true
 	else:
 		marker.visible = false
+		
+	# 2. OBSŁUGA OBROTU SKOKOWEGO (Prawy joystick)
+	var joystick: Vector2 = get_vector2("thumbstick")
+	if abs(joystick.x) > 0.5: # Wychylenie powyżej 50%
+		if can_snap_turn:
+			snap_turn(sign(joystick.x))
+			can_snap_turn = false # Blokujemy kolejne obrócenie
+	elif abs(joystick.x) < 0.2: # Puszczenie drążka
+		can_snap_turn = true # Odblokowujemy obrót
 
-# TA FUNKCJA ODPALA TELEPORT PO KLIKNIĘCIU SPUSTU:
-func _on_button_pressed(button_name: String) -> void:
+func _on_button_released(button_name: String) -> void:
 	if button_name == "trigger_click" or button_name == "trigger":
 		teleport_now()
+
+func snap_turn(direction: float) -> void:
+	# Obrót uwzględniający offset głowy (kamera zostaje w miejscu)
+	var cam_pos = xr_camera.global_position
+	xr_origin.rotate_y(-direction * turn_step)
+	var new_cam_pos = xr_camera.global_position
+	xr_origin.global_position += (cam_pos - new_cam_pos)
 
 func teleport_now() -> void:
 	if not ray.is_colliding():
 		return
+		
 	var target: Vector3 = ray.get_collision_point()
-
-	var origin_tf := xr_origin.global_transform
-	var cam_tf := xr_camera.global_transform
-	var cam_offset := cam_tf.origin - origin_tf.origin
-
-	cam_offset.y = 0.0
-	origin_tf.origin = Vector3(target.x - cam_offset.x, target.y, target.z - cam_offset.z)
-	xr_origin.global_transform = origin_tf
+	var origin_pos := xr_origin.global_position
+	var cam_pos := xr_camera.global_position
+	
+	# Obliczamy wektor przesunięcia między środkiem pokoju a głową (offset)
+	var cam_offset := cam_pos - origin_pos
+	
+	# Ignorujemy pozycję na osi Y, żeby gracz nie zapadł się pod ziemię lub nie lewitował
+	cam_offset.y = 0.0 
+	
+	# Ustalamy nową pozycję: celujemy głową w target, więc środek pokoju musi być przesunięty o offset
+	xr_origin.global_position = Vector3(target.x - cam_offset.x, target.y, target.z - cam_offset.z)
